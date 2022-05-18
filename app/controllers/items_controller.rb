2f8b97a34182a76b1fa69d99b1642c798e6bad6f
clair13 <theclairvoyant1313@yahoo.gr>
2022-05-18 19:45:52 +0300

    Update items table and add more links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,6 +65,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :quantity, :date, :description)
+      params.require(:item).permit(:name, :quantity, :date, :description, :location_id)
     end
 end

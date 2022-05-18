989e2d6cd1d2504936108ee78ac2247b72d6c13b
clair13 <theclairvoyant1313@yahoo.gr>
2022-05-18 19:08:22 +0300

    Create migration AddLocationToItems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_150408) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_160241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_150408) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "location_id"
+    t.index ["location_id"], name: "index_items_on_location_id"
   end
 
   create_table "locations", force: :cascade do |t|
